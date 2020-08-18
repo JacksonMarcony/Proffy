43bb5abe93d2dbccc8de49247b96732762514f9a
JacksonMarcony <jacklujack23@gmail.com>
2020-08-18 02:01:33 -0300

    create: pages of frontend aplication class three

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import Logo from '../../assets/images/logo.svg'
@@ -6,10 +6,21 @@ import LandingImg from '../../assets/images/landing.svg'
 import StudyIcon from '../../assets/images/icons/study.svg'
 import GiveClassIcon from '../../assets/images/icons/give-classes.svg'
 import PurpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import api from '../../services/api';
 
 import './styles.css'
 
 function Landing() {
+	const [totalConnections, setTotalConnections] = useState(0);
+
+	useEffect(() => {
+		api.get('connections').then(response => {
+			const { total } = response.data;
+
+			setTotalConnections(total);
+		})
+	}, []);
+
   return (
 		<div id="page-landing">
 			<div id="page-landing-content" className="container">
@@ -36,7 +47,7 @@ function Landing() {
 					</Link>
 				</div>
 				<span className="total-connections">
-						Total de 200 conexões já realizadas <img src={PurpleHeartIcon} alt="Coração roxo"/>
+						Total de {totalConnections} conexões já realizadas <img src={PurpleHeartIcon} alt="Coração roxo"/>
 				</span>
 			</div>
 		</div>
